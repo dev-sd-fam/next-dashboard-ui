d4c7e89a3bc051f2989a8e6966949e942373927e
subodh <subodhd@axioned.com>
2024-11-17 19:03:30 +0530

    FE ui done

--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -1,7 +1,25 @@
 "use client";
 
+import dynamic from "next/dynamic";
 import Image from "next/image";
 import { useState } from "react";
+// import TeacherForm from "./forms/TeacherForm";
+// import StudentForm from "./forms/StudentForm";
+
+const TeacherForm = dynamic(() => import("./forms/TeacherForm"), {
+  loading: () => <h2>Loading...</h2>,
+});
+
+const StudentForm = dynamic(() => import("./forms/StudentForm"), {
+  loading: () => <h2>Loading...</h2>,
+});
+
+const forms: {
+  [key: string]: (type: "create" | "update", data?: any) => JSX.Element;
+} = {
+  teacher: (type, data) => <TeacherForm type={type} data={data} />,
+  student: (type, data) => <StudentForm type={type} data={data} />,
+};
 
 const FormModal = ({
   table,
@@ -46,8 +64,10 @@ const FormModal = ({
           Delete
         </button>
       </form>
+    ) : type === "create" || type === "update" ? (
+      forms[table](type, data)
     ) : (
-      "create or update form"
+      "Form not found"
     );
   };
 
